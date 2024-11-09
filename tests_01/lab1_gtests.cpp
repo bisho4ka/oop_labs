37bc8876bd527f782745c0ca64d4337a212043a6
anastasiabudanova <114395124+bisho4ka@users.noreply.github.com>
2024-11-09 06:09:42 +0300

    Create lab1_gtests.cpp

---
+++ tests_01/lab1_gtests.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include "../include/checkingTheString.h"
+
+TEST(tests_lab1, EmptyString) {
+  std::string str = "";
+  bool result = checkingTheString(str);
+  EXPECT_EQ(result, true);
+}
+
+TEST(tests_lab1, CorrectString) {
+  std::string str = "()()()";
+  bool result = checkingTheString(str);
+  EXPECT_EQ(result, true);
+}
+
+TEST(tests_lab1, CorrectString) {
+  std::string str = "(()()(()))";
+  bool result = checkingTheString(str);
+  EXPECT_EQ(result, true);
+}
+
+TEST(tests_lab1, IncorrectString) {
+  std::string str = ")())()";
+  bool result = checkingTheString(str);
+  EXPECT_EQ(result, false);
+}
+
+TEST(tests_lab1, LongString) {
+  std::string str = "((()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()))";
+  ASSERT_THROW(checkingTheString(str), std::invalid_argument);
+}
+
+TEST(tests_lab1, OtherChar) {
+  std::string str = "()(((hi)))()";
+  ASSERT_THROW(checkingTheString(str), std::invalid_argument);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
