c7c34a18c5258f19425df58b98c028eec82d3aad
anastasiabudanova <114395124+bisho4ka@users.noreply.github.com>
2024-11-12 10:45:57 +0300

    Update checkingTheString.cpp

--- src/checkingTheString.cpp
+++ src/checkingTheString.cpp
@@ -3,7 +3,7 @@
 bool checkingTheString(const std::string &str) {
     int open = 0, close = 0;
     
-    if (str.length() > 100) throw std::invalid_argument("String limit");
+    if (str.length() > 100) throw std::length_error("String limit");
     if (str.length() == 0) return true;
 
     for (int i = 0; i < str.length(); i++) {
